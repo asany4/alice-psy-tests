fe4d03a66936a1cf5f7c91c8e6456c29c0af153e
asany4 <asany4@yandex-team.ru>
2020-02-01 16:30:17 +0300

    Добавил сам тест

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const { Alice, Reply, Markup } = require('yandex-dialogs-sdk')
+const { AgeTest } = require('./age');
+
 const alice = new Alice();
 
+const sessions = {};
+
 const M = Markup;
-alice.command('', async ctx => Reply.text('Пройдите психологические тесты. Они помогут познать себя лучше. Пока что я не умею проводить тесты, но совсем скоро меня научат. Возвращайтесь немного позже!'));
-alice.command('Give a piece of advice', async ctx =>
-  Reply.text('Make const not var'),
-);
-alice.command(
-  ['What is trending now?', 'Watch films', 'Whats in the theatre?'],
-  ctx => {
-    return {
-      text: `What about 50 Angry Men?`,
-      buttons: [M.button('Buy ticket'), M.button('What else?')],
-    };
-  },
-);
-alice.command(/(https?:\/\/[^\s]+)/g, ctx => Reply.text('Matched a link!'));
-alice.any(async ctx => Reply.text(`Я не понимаю`));
+alice.command('', async ctx => Reply.text('Пройдите тест "Сколько я проживу". Я буду задавать вопросы, и в зависимости от ваших ответов я посчитаю, сколько в среднем живут люди с таким же образом жизни. Хотите запустить тест?'));
+alice.command(['запустить', 'начать'], async ctx => Reply.text('Тут должен быть первый вопрос'));
+// alice.command('Give a piece of advice', async ctx =>
+//   Reply.text('Make const not var'),
+// );
+// alice.command(
+//   ['What is trending now?', 'Watch films', 'Whats in the theatre?'],
+//   ctx => {
+//     return {
+//       text: `What about 50 Angry Men?`,
+//       buttons: [M.button('Buy ticket'), M.button('What else?')],
+//     };
+//   },
+// );
+// alice.command(/(https?:\/\/[^\s]+)/g, ctx => Reply.text('Matched a link!'));
+// alice.any(async ctx => Reply.text(`Я не понимаю`));
+
+alice.any(async ctx => {
+  sessions[ctx.sessionId] = sessions[ctx.sessionId] || {};
+  try {
+      answer = AgeTest(ctx, sessions[ctx.sessionId]);
+  } catch(e) {
+      console.error(e);
+      answer = 'Произошла ошибка в навыке. Попробуйте начать тест снова';
+  };
+  return Reply.text(answer);
+});
 
 const server = alice.listen(process.env.PORT || 3001, '/');
 
